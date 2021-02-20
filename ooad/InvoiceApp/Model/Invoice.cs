4f5db78020da360f26dc6ee59c47e4797a6a71ec
ranchcoder1996 <vishalrs2611@gmail.com>
2021-02-20 17:04:57 +0530

    invoice app

---
+++ ooad/InvoiceApp/Model/Invoice.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InvoiceApp.Model
+{
+    class Invoice
+    {
+        private int _id;
+        private string _name;
+        private double _discount;
+        private double _gst;
+        private double _amount;
+
+        public Invoice(int id, string name, double discount, double gst, double amount)
+        {
+            _id = id;
+            _name = name;
+            _discount = discount;
+            _gst = gst;
+            _amount = amount;
+        }
+
+        public int Id { get => _id; }
+        public string Name { get => _name;  }
+        public double Discount { get => _discount;  }
+        public double Gst { get => _gst;  }
+        public double Amount { get => _amount;}
+
+
+        public double ComputeDiscount()
+        {
+            return _amount * Discount;
+        }
+
+        public double ComputeGst()
+        {
+            return _amount * Gst;
+        }
+
+        public double CalculateTotalCost()
+        {
+            return (_amount - ComputeDiscount()) + ComputeGst();
+        }
+    }
+}
---
+++ ooad/InvoiceApp/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using InvoiceApp.Model;
+namespace InvoiceApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Invoice invoiceMessage = new Invoice(1,"Vishal Singh",0.024,0.18,1000);
+
+            PrintInvoiceDetail(invoiceMessage);
+            Console.ReadLine();
+        }
+
+        public static void PrintInvoiceDetail(Invoice invoiceMessage)
+        {
+            Console.WriteLine($"Name : {invoiceMessage.Name}\nAmount : Rs{invoiceMessage.Amount}/-\nDiscount :Rs{invoiceMessage.ComputeDiscount()}/-" +
+                $"\nG.S.T :Rs{invoiceMessage.ComputeGst()}/-" +
+                $"\nFinal Amount :Rs{invoiceMessage.CalculateTotalCost()}/-");
+            
+
+        }
+    }
+}
