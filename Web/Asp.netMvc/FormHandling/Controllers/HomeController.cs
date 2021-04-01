6918f3ca3ec35c5905f8b0837c461bb639e55186
ranchcoder1996 <vishalrs2611@gmail.com>
2021-04-01 11:50:29 +0530

    asp.net projects added

---
+++ Web/Asp.netMvc/FormHandling/Controllers/HomeController.cs
@@ -0,0 +1,84 @@
+﻿using FormHandling.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace FormHandling.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult FetchFormData(FormCollection collection)
+        {
+            ViewBag.Name = new List<string>()
+            {
+                collection[0],
+                collection[0],
+                collection[0],
+                collection[0],
+                collection[0],
+
+            };
+            return View();
+        }
+
+        public ActionResult FetchMultipleData(FormCollection collection)
+        {
+            ViewData["Id"] = collection[0];
+            ViewData["Name"] = collection[1];
+            return View();
+        }
+
+        public ActionResult Timer()
+        {
+            int hourOfDay = DateTime.Now.Hour;
+            
+            if(hourOfDay > 5 && hourOfDay <= 12)
+            {
+                ViewBag.Greet = "Good Morning";
+            } else if(hourOfDay >= 12 && hourOfDay <= 18)
+            {
+                ViewBag.Greet = "Good Afternoon";
+            }else if(hourOfDay >= 18 && hourOfDay <= 23)
+            {
+                ViewBag.Greet = "Good Evening";
+            }
+            else
+            {
+                ViewBag.Greet = "Good Night";
+            }
+
+            return View();
+        }
+
+        public ActionResult ModelDataFetching()
+        {
+
+           
+
+            return View();
+
+        }
+
+        public ActionResult DynamicTextBox(FormCollection collection)
+        {
+            ViewData["userId"] = collection[0];
+            ViewData["userName"] = collection[1];
+            return View();
+        }
+
+        public ActionResult FormDataDropDown(FormCollection collection)
+        {
+            ViewData["dropdown_value"] = collection[0];
+            return View();
+        }
+    }
+}
---
+++ Web/Asp.netMvc/WebApplication2/Controllers/HomeController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebApplication2.Controllers
+{
+    public class HomeController : Controller
+    {
+        public HomeController()
+        {
+
+        }
+
+        public ActionResult Index(string name)
+        {
+            ViewBag.Names = new List<string>()
+           {
+               name,
+               name,
+               name,
+               name,
+               name
+           };
+            return View();
+        }
+
+        public ActionResult Hello()
+        {
+            return View();
+        }
+
+        
+    }
+}
